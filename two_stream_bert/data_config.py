92fa9173da61c720bb5901b0de81a1d9c90bd5d5
hoya012 <hoseong.lee@cognex.com>
2021-04-02 13:37:48 +0900

    seperate data locations

---
+++ two_stream_bert/data_config.py
@@ -0,0 +1,22 @@
+#FIXME
+def get_dataset(args):
+    if args.dataset=='ucf101':
+        dataset='./datasets/ucf101_frames'
+    elif args.dataset=='hmdb51':
+        dataset='./datasets/hmdb51_frames'
+    elif args.dataset=='smtV2':
+        dataset='./datasets/smtV2_frames'
+    elif args.dataset=='window':
+        dataset='./datasets/window_frames'
+    elif args.dataset=='cvpr':
+        if args.light_enhanced:
+            dataset='/data_hdd/hoseong/ARID_Enhanced/cvpr_frames_enhanced'
+        else:
+            dataset='/data_hdd/hoseong/ARID/cvpr_frames'
+    elif args.dataset=='cvpr_le':
+        dataset='./datasets/cvpr_le_frames'
+    else:
+        print("No convenient dataset entered, exiting....")
+        return 0
+
+    return dataset
--- two_stream_bert/data_prep.py
+++ two_stream_bert/data_prep.py
@@ -26,27 +26,6 @@ def get_size(args):
 
     return input_size, width, height
 
-def get_dataset(args):
-    if args.dataset=='ucf101':
-        dataset='./datasets/ucf101_frames'
-    elif args.dataset=='hmdb51':
-        dataset='./datasets/hmdb51_frames'
-    elif args.dataset=='smtV2':
-        dataset='./datasets/smtV2_frames'
-    elif args.dataset=='window':
-        dataset='./datasets/window_frames'
-    elif args.dataset=='cvpr':
-        if args.light_enhanced:
-            dataset='./datasets/cvpr_frames_enhanced'
-        else:
-            dataset='./datasets/cvpr_frames'
-    elif args.dataset=='cvpr_le':
-        dataset='./datasets/cvpr_le_frames'
-    else:
-        print("No convenient dataset entered, exiting....")
-        return 0
-
-    return dataset
 
 def get_data_stat(args):
     modality = args.arch.split('_')[0]
