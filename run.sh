f9f571b6583fc00913bc6e70e2cc1cfe1472896c
hyeokjae-choi <hyeokjae94@gmail.com>
2021-04-23 15:29:44 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -9,3 +9,6 @@ python two_stream_bert2.py --split=51 --arch=flow_resneXt3D64f101_bert10_FRMB --
 python two_stream_bert2.py --split=51 --arch=rgb_r2plus1d_64f_34_bert10 --workers=8 --batch-size=2 --iter-size=16 --print-freq=400 --save_dir=/data/hyeokjae/trained/workshop --lr=1e-5 --gpu 1 --dataset=cvpr
 python two_stream_bert2.py --split=51 --arch=rgb_r2plus1d_64f_34_bert10 --workers=8 --batch-size=2 --iter-size=16 --print-freq=400 --save_dir=/data/hyeokjae/trained/workshop --lr=1e-5 --gpu 2 --dataset=cvpr_sid
 python two_stream_bert2.py --split=51 --arch=rgb_r2plus1d_64f_34_bert10 --workers=8 --batch-size=2 --iter-size=16 --print-freq=400 --save_dir=/data/hyeokjae/trained/workshop --lr=1e-5 --gpu 3 --dataset=cvpr_sid_sm
+
+python two_stream_bert2.py --split=51 --arch=rgb_r2plus1d_64f_34_bert10 --workers=8 --batch-size=2 --iter-size=16 --print-freq=400 --save_dir=/data/hyeokjae/trained/workshop --lr=1e-5 --gpu 1 --dataset=cvpr --randaug 2_3
+python two_stream_bert2.py --split=51 --arch=rgb_r2plus1d_64f_34_bert10 --workers=8 --batch-size=2 --iter-size=16 --print-freq=400 --save_dir=/data/hyeokjae/trained/workshop --lr=1e-5 --gpu 2 --dataset=cvpr_sid_sm --randaug 2_3
